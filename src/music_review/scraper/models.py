7f1da27b568d43d5426f251304c247e9ecd60e15
Thomas Plischke <t.plischke@web.de>
2026-02-28 17:25:07 +0100

    deletes unnessary folders

--- src/music_review/scraper/models.py
+++
@@ -1,7 +0,0 @@
-# src/music_review/scraper/models.py
-
-"""Re-export domain models for backward compatibility."""
-
-from music_review.domain.models import Review, Track
-
-__all__ = ["Review", "Track"]
